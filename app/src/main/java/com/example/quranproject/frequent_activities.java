454d30f93310ef462ab1bcc1ce40783149dc1935
abdullahek <tomsteven86>
2019-08-25 22:23:50 +0500

    Initial commit

---
+++ app/src/main/java/com/example/quranproject/frequent_activities.java
@@ -0,0 +1,13 @@
+package com.example.quranproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class frequent_activities extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frequent_activities);
+    }
+}
