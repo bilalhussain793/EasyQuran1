fbd81b1de82b209d6c4c38ce6d9832e778122f1e
abdullahek <tomsteven86>
2019-08-25 22:41:14 +0500

    Activities combined

--- app/src/main/java/com/example/quranproject/MainActivity.java
+++ app/src/main/java/com/example/quranproject/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.quranproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_create_act,btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_create_act = (Button) findViewById(R.id.create_btn);
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        btn_create_act.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent I = new Intent(getApplicationContext(),learning_goals.class);
+            }
+
+        });
+        btn_login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent I = new Intent(getApplicationContext(),activity_login.class);
+            }
+
+        });
+
+
     }
 }
--- app/src/main/java/com/example/quranproject/learning_goals.java
+++ app/src/main/java/com/example/quranproject/learning_goals.java
@@ -1,13 +1,27 @@
 package com.example.quranproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class learning_goals extends AppCompatActivity {
+    Button btn_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning_goals);
+        btn_next = (Button) findViewById(R.id.btn_next);
+
+        btn_next.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent I = new Intent(getApplicationContext(),frequent_activities.class);
+            }
+
+        });
     }
 }
